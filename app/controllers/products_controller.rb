f73ca60611bfc4310bea646319a89954f6b0874c
mrDani <darnnielnology@gmail.com>
2025-03-26 14:36:51 -0500

    added search by category functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,18 @@
 class ProductsController < ApplicationController
   def index
-    @q = Product.ransack(params[:q])
-    @products = @q.result.page(params[:page]).per(10)
-    @categories = Category.all
+    @products = Product.all
+  
+    if params[:query].present?
+      @products = @products.where("name ILIKE ?", "%#{params[:query]}%")
+    end
+  
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
+  
+    @products = @products.page(params[:page]).per(12)
   end
+  
 
   def show
     @product = Product.find(params[:id])
