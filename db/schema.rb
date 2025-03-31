d0589f5f8dbbfc3fcaa01cba58000c31417909f4
mrDani <darnnielnology@gmail.com>
2025-03-30 21:37:41 -0500

    added username

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_26_151449) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_31_023322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,8 +158,10 @@ ActiveRecord::Schema[7.2].define(version: 2025_03_26_151449) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.string "province"
+    t.string "username", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
