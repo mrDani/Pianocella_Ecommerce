164f06b83d2ef9c11b91e730adb202cecc01ea4d
mrDani <darnnielnology@gmail.com>
2025-03-30 21:31:58 -0500

    fixed products not found issue

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show]
+
   def index
     @products = Product.all
   
@@ -9,12 +11,25 @@ class ProductsController < ApplicationController
     if params[:category_id].present?
       @products = @products.where(category_id: params[:category_id])
     end
+
+    # Order from newest to oldest
+    @products = @products.order(created_at: :desc)
   
+    # Paginate products
     @products = @products.page(params[:page]).per(12)
   end
   
-
   def show
-    @product = Product.find(params[:id])
+    # The @product is already set by the set_product method below.
+  end
+
+  private
+
+  def set_product
+    @product = Product.find_by(id: params[:id])
+    if @product.nil?
+      redirect_to products_path, alert: "Product not found or has been deleted."
+    end
   end
 end
+
